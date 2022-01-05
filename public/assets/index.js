6e514ff562e59440e4d43adbae2487f77a68e2c3
unknown <tyronejbayasen@gmail.com>
2022-01-05 10:43:59 -0800

    item dynamically created in HTML

--- public/assets/index.js
+++ public/assets/index.js
@@ -1,29 +1,59 @@
-//Grab data from text input box
-//Initialize Variables
-let inputItem = document.getElementById("inputText");
-let submitItem = document.getElementById("submit-btn");
-const todos = [];
-let id = 0;
+// grab data from text input box
+// initialize variables
+let todos = []
+,id = 0;
 
-//console log text
-let addItem = function(inputItem) {
+// HTML element variables
+const inputItem = document.getElementById("inputText");
+const submitItem = document.getElementById("submit-btn");
+const list = document.getElementById("list-table")
+
+// get data from localstorage
+let data = localStorage.getItem("TODO");
+
+// check if data in local storage
+if(data) {
+    todos = JSON.parse(data);
+    id = todos.length;
+}
+
+// store data in array and save to local storage
+let storeItem = function(inputItem) {
     inputItem = document.getElementById("inputText").value;
-    todos.push({"name": inputItem, "id": id,"done": false, "trash": false });
-    console.log(todos);
+    // localStorage.setItem("TODO",JSON.stringify(todos))
+    // console.log(data);
+    if (inputItem) {
+        addItem(inputItem, id, false, false);
+        todos.push({"name": inputItem, "id": id,"done": false, "trash": false });
+        console.log(inputItem, id, false, false)
+    }
     id++;
 }
 
-submitItem.addEventListener("click", addItem);
+submitItem.addEventListener("click", storeItem);
 inputItem.addEventListener("keyup", function(event) {
     if (event.keyCode == 13) {
         event.preventDefault();
         submitItem.click();
     }
 });
-//store text
 
-//function to generate list-item upon item creation
+// function to generate list-item upon item creation
+function addItem(listItem) {
+    const element = `<li class="list-item"><input type="checkbox" id="${id}"
+    action="complete"><span class="item-text">${listItem}</span><i class="fas fa-trash-alt trash-icon" action="delete"></i></li>`
+    const position = 'beforeend'
 
-//function to mark item as done
+    list.insertAdjacentHTML(position, element)
+}
 
-//function to delete/trash item
+list.addEventListener("click", function(event) {
+    let element = event;
+    console.log(element)
+})
+
+// function to mark item as done
+// click event when item is checked should change action to "done" to true
+
+// function to delete/trash item
+// click event should change "trash" to true and delete item
